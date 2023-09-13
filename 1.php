7455c18a5aaa7f5d8e304edbd627354e57212cbe
bartekwiorek <114527953+bartekwiorek@users.noreply.github.com>
2023-09-13 11:46:10 +0200

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,23 @@
+<?php
+$a=2;
+$b=2;
+
+$c=$a + $b;
+echo"$a + $b = $c </br>";
+
+$c=$a - $b;
+echo"$a - $b = $c </br>";
+
+$c=$a * $b;
+echo"$a * $b = $c </br>";
+
+$c=$a / $b;
+echo"$a / $b = $c </br>";
+
+$c=$a % $b;
+echo"$a mod $b = $c </br>";
+
+$c=$a * $b;
+echo"$a * $b = $c </br>";
+
+?>
