795719e680e09e61e1dd837078fbb54944972a71
bartekwiorek <114527953+bartekwiorek@users.noreply.github.com>
2023-09-13 12:19:00 +0200

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,16 @@
+<?php
+$a=10;
+$b=30;
+
+$c=$a * $a;
+echo"Pole kwadratu: $a * $a = <b><font color='red'>$c</font></b></br>";
+
+$c=$a * $b;
+echo"Pole prostokąta: $a * $b = <b><font color='red'>$c</font></b></br>";
+
+$c=($a * $b) / 2;
+echo"Pole trójkąta: $a * $b / 2 = <b><font color='red'>$c</font></b></br>";
+
+$c=($a*$a * 3.14);
+echo"Pole okręgu: $a^2 * 3.14 = <b><font color='red'>$c</font></b></br>";
+?>
