f860e517083248db36938e085aece001e3394f7a
bartekwiorek <114527953+bartekwiorek@users.noreply.github.com>
2023-09-21 09:09:13 +0200

    Add files via upload

---
+++ 3.php
@@ -0,0 +1,42 @@
+<?php
+    $a=12;
+
+    if ($a % 2 == 0) {
+        echo"Liczba jest podzielna przez 2<br>";
+        $b = $a / 2;
+        echo"Wynik działania to $b<br><br>";
+    }
+
+    if($a % 3 == 0 ) {
+        echo"Liczba jest podzielna przez 3<br>";
+        $b = $a / 3;
+        echo"Wynik działania to $b<br><br>";
+    }
+
+    if($a % 4 == 0 ) {
+        echo"Liczba jest podzielna przez 4<br>";
+        $b = $a / 4;
+        echo"Wynik działania to $b<br><br>";
+    }
+
+    if($a % 5 == 0 ) {
+        echo"Liczba jest podzielna przez 5<br>";
+        $b = $a / 5;
+        echo"Wynik działania to $b<br><br>";
+    }
+
+    if($a % 6 == 0 ) {
+        echo"Liczba jest podzielna przez 6<br>";
+        $b = $a / 6;
+        echo"Wynik działania to $b<br><br>";
+    }
+
+    
+    if($a % 7 == 0 ) {
+        echo"Liczba jest podzielna przez 7<br>";
+        $b = $a /    7;
+        echo"Wynik działania to $b<br><br>";
+    }
+
+
+?>
