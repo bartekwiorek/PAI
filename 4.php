040076422e935693ed67995c32f03b77d28f910c
bartekwiorek <114527953+bartekwiorek@users.noreply.github.com>
2023-09-24 18:53:49 +0200

    Update 4.php

--- 4.php
+++ 4.php
@@ -2,36 +2,38 @@
 
 $a=0;
 $b=5;
-$c=2;
-$d=1;
-$e=0;
-$f=7;
-$g=4;
-$h=8;
-$i=7;
-$j=1;
+$c=3;
+$d=0;
+$e=1;
+$f=9;
+$g=0;
+$h=6;
+$i=3;
+$j=5;
+
 $k=1;
+$l=3;
+$m=7;
+$n=9;
 
-
-$liczenie1= $a * 1 + $b * 3 + $c * 7 + $d * 9 + $e * 1 + $f * 3 + $g * 7 + $h * 9 + $i * 1 + $j * 3; 
-$dziel10 = $liczenie1 % 10;
-$kontrolna = 10 - $dziel10;
-$ostcyfra = $kontrolna;
+$liczenie1= $a * $k + $b * $l + $c * $m + $d * $n + $e * $k + $f * $l + $g * $m + $h * $n + $i * $k + $j * $l; 
+$mod = $liczenie1 % 10;
+$lkon = 10 - $mod;
+$ostcyf = $lkon;
  
 if ($j%2==1)
 {
-    echo"Płeć: Chłop<br>";
+    echo"Men</br>";
 }
 else{
-    echo"Płeć: Baba<br>";
-};
-
-echo"Ostatnia cyfra peselu $a $b $c $d $e $f $g $h $i $j $k wynosi $ostcyfra";
-
-if ($k == $ostcyfra){
-    echo"<br>Pesel jest poprawny";
-}else
-{
-    echo"<br>Pesel nie jest poprawny";
+    echo"Women</br>";
 }
+
+echo"Ostatnia cyfra peselu $a $b $c $d $e $f $g $h $i $j wynosi $ostcyf";
+
+echo"</br>Rok urodzenia 20$a$b";
+$miesiac = 4 - $c.$d ;
+echo"</br>Miesiąc $miesiac";
+echo"</br>Dzień $e$f";
+
 ?>
