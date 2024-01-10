b3e8961918f202cccbd90a889d4b286fc9b7deca
bartekwiorek <114527953+bartekwiorek@users.noreply.github.com>
2024-01-10 12:25:42 +0100

    Update 10.php

--- 10.php
+++ 10.php
@@ -1,17 +1,29 @@
 <?php
 $a = "";
 $b = "";
-$suma = "";
+$wynik = "";
 
 if (isset($_POST['a'])) {
     $a = $_POST['a'];
     $b = $_POST['b'];
-    $suma = $a + $b;
+    $wynik = $a * $b;
 }
-echo "
+echo"
 <form name='dane' action='' method='post'>
-        <input type='a' name='a' min='0' max='100' value = '$a'> +
-        <input type='b' name='b' min='0' max='100'value = '$b'> = $suma
+        <input type='number' name='a' min='0' max='100' value = '$a'> x
+        <input type='number' name='b' min='0' max='100'value = '$b'> = $wynik
         <input type='SUBMIT' name='zatwierdz' value = 'OK'>
 </form>";
+echo"<table border=1>";
+
+for($i=1;$i<=$a;$i++) {
+    echo"<tr><td>$i</td>";
+for($j=2;$j<=$b;$j++) {
+	echo"<td>$j</TD>";
+    }	   
+    echo"</TR>";
+    }
+echo"</TABLE>";
+
+
 ?>
