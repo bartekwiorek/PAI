f223f63e4fdb5b1cce708e853ac93a3f7d29703c
bartekwiorek <114527953+bartekwiorek@users.noreply.github.com>
2024-01-16 20:33:14 +0100

    Update 10.php

--- 10.php
+++ 10.php
@@ -10,8 +10,8 @@ if (isset($_POST['a'])) {
 }
 echo"
 <form name='dane' action='' method='post'>
-        <input type='number' name='a' min='0' max='100' value = '$a'> x
-        <input type='number' name='b' min='0' max='100'value = '$b'> = $wynik
+        <input type='select' name='a' min='0' max='100' value = '$a'> x
+        <input type='select' name='b' min='0' max='100'value = '$b'> = $wynik
         <input type='SUBMIT' name='zatwierdz' value = 'OK'>
 </form>";
 echo"<table border=1>";
@@ -19,7 +19,8 @@ echo"<table border=1>";
 for($i=1;$i<=$a;$i++) {
     echo"<tr><td>$i</td>";
 for($j=2;$j<=$b;$j++) {
-	echo"<td>$j</TD>";
+    $c=$i*$j;
+	echo"<td>$c</TD>";
     }	   
     echo"</TR>";
     }
