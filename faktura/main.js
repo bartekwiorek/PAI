166d9718177c8ccd402150b2ed7a62911f9a05ca
bartekwiorek <114527953+bartekwiorek@users.noreply.github.com>
2024-10-15 12:18:06 +0200

    Add files via upload

--- faktura/main.js
+++ faktura/main.js
@@ -33,26 +33,89 @@ function wystawca() {
     wystawc.innerHTML= "Wystawca: <b>" + wyst + "</b>"
 }
 function ceny() {
-    nazwa_towaru = document.getElementById("nazwa_towaru").value
-    td2.innerHTML = nazwa_towaru
+    nazwa_towaru1 = document.getElementById("nazwa_towaru1").value
+    nazwa_towaru2 = document.getElementById("nazwa_towaru2").value
+    nazwa_towaru3 = document.getElementById("nazwa_towaru3").value
+    nazwa_towaru4 = document.getElementById("nazwa_towaru4").value
+    nazwa_towaru5 = document.getElementById("nazwa_towaru5").value
+    td2.innerHTML = nazwa_towaru1
+    td22.innerHTML = nazwa_towaru2
+    td222.innerHTML = nazwa_towaru3
+    td2222.innerHTML = nazwa_towaru4
+    td22222.innerHTML = nazwa_towaru5
 
-    ilosc = document.getElementById("ilosc").value
-    ilosc = parseInt(ilosc)
-    td3.innerHTML = ilosc
+    ilosc1 = document.getElementById("ilosc1").value
+    ilosc1 = parseInt(ilosc1)
+    ilosc2 = document.getElementById("ilosc2").value
+    ilosc2 = parseInt(ilosc2)
+    ilosc3 = document.getElementById("ilosc3").value
+    ilosc3 = parseInt(ilosc3)
+    ilosc4 = document.getElementById("ilosc4").value
+    ilosc4 = parseInt(ilosc4)
+    ilosc5 = document.getElementById("ilosc5").value
+    ilosc5 = parseInt(ilosc5)
+    td3.innerHTML = ilosc1
+    td33.innerHTML = ilosc2
+    td333.innerHTML = ilosc3
+    td3333.innerHTML = ilosc4
+    td33333.innerHTML = ilosc5
 
-    cena_jednostkowa = document.getElementById("cena_jednostkowa").value
-    cena_jednostkowa = parseFloat(cena_jednostkowa)
-    td4.innerHTML = cena_jednostkowa + "zł"
+    cena_jednostkowa1 = document.getElementById("cena_jednostkowa1").value
+    cena_jednostkowa1 = parseFloat(cena_jednostkowa1)
+    cena_jednostkowa2 = document.getElementById("cena_jednostkowa2").value
+    cena_jednostkowa2 = parseFloat(cena_jednostkowa2)
+    cena_jednostkowa3 = document.getElementById("cena_jednostkowa3").value
+    cena_jednostkowa3 = parseFloat(cena_jednostkowa3)
+    cena_jednostkowa4 = document.getElementById("cena_jednostkowa4").value
+    cena_jednostkowa4 = parseFloat(cena_jednostkowa4)
+    cena_jednostkowa5 = document.getElementById("cena_jednostkowa5").value
+    cena_jednostkowa5 = parseFloat(cena_jednostkowa5)
+    td4.innerHTML = cena_jednostkowa1 + "zł"
+    td44.innerHTML = cena_jednostkowa2 + "zł"
+    td444.innerHTML = cena_jednostkowa3 + "zł"
+    td4444.innerHTML = cena_jednostkowa4 + "zł"
+    td44444.innerHTML = cena_jednostkowa5 + "zł"
 
-    w_netto = 0
-    w_netto = parseFloat(w_netto)
-    w_netto = cena_jednostkowa * ilosc
-    ww_netto = w_netto
-    td5.innerHTML = ww_netto + "zł"
+    w_netto1 = 0
+    w_netto2 = 0
+    w_netto3 = 0
+    w_netto4 = 0
+    w_netto5 = 0
+    w_netto1 = parseFloat(w_netto1)
+    w_netto2 = parseFloat(w_netto2)
+    w_netto3 = parseFloat(w_netto3)
+    w_netto4 = parseFloat(w_netto4)
+    w_netto5 = parseFloat(w_netto5)
+    w_netto1 = cena_jednostkowa1 * ilosc1
+    w_netto2 = cena_jednostkowa2 * ilosc2
+    w_netto3 = cena_jednostkowa3 * ilosc3
+    w_netto4 = cena_jednostkowa4 * ilosc4
+    w_netto5 = cena_jednostkowa5 * ilosc5
+    ww_netto1=w_netto1
+    ww_netto2=w_netto2
+    ww_netto3=w_netto3
+    ww_netto4=w_netto4
+    ww_netto5=w_netto5
+
+    td5.innerHTML = ww_netto1 + "zł"
+    td55.innerHTML = ww_netto2 + "zł"
+    td555.innerHTML = ww_netto3 + "zł"
+    td5555.innerHTML = ww_netto4 + "zł"
+    td55555.innerHTML = ww_netto5 + "zł"
 }
 function vaty() {
-    let vat = document.getElementById("stawka_vat").value
-    vat = parseInt(vat)
+    let vat1 = document.getElementById("stawka_vat1").value
+    vat1 = parseInt(vat1)
+    let vat2 = document.getElementById("stawka_vat2").value
+    vat2 = parseInt(vat2)
+    let vat3 = document.getElementById("stawka_vat3").value
+    vat3 = parseInt(vat3)
+    let vat4 = document.getElementById("stawka_vat4").value
+    vat4 = parseInt(vat4)
+    let vat5 = document.getElementById("stawka_vat5").value
+    vat5 = parseInt(vat5)
+
+    //tutaj
     kwota_vat = 3
     kwota_vat = parseFloat(kwota_vat)
         if (vat == "23") {
