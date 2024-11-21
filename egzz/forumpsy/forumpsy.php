440b7c369c43f1190833e02b384060e178ec1f97
bartekwiorek <114527953+bartekwiorek@users.noreply.github.com>
2024-11-21 11:30:42 +0100

    Add files via upload

---
+++ egzz/forumpsy/forumpsy.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum o psach</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <section id=header>Forum miłośników psów</section>
+    <section id=main>
+        <section id=left>
+            <img src="avatar.jpg">
+            <video controls loop>
+                <source src="video.mp4" type="video/mp4">
+            </video>
+        </section>
+        <section id=right>
+            <form>
+                <textarea></textarea>
+                <button>Dodaj odpowiedź</button>
+            </form>
+            <h2>Odpowiedzi na pytanie</h2>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </section>
+    </section>
+    <section id="stopka">
+        Autor: 21i3123123 <a href="http://mojestrony.pl/" blank_>Zobacz nasze realizacje</a>
+    </section>
+</body>
+</html>
